6bec7554f4a4721c835440a5bc23f297f5ecb0f2
Nikkoola22 <nikkoola@gmail.com>
2025-09-23 18:17:43 +0200

    Clean repo: removed API key history

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  base: './', // Gardez ceci si nécessaire pour les chemins
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+  base: '/ATLAS/', // ← AJOUTE CETTE LIGNE
+});
